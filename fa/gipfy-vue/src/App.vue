7f96ee25d66460c0cf1f4dcbf2d610bfa2dcead4
madebyrh <madebyrh@gmail.com>
2019-03-22 06:38:37 +0900

    FirstCommit

---
+++ fa/gipfy-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- ここに先ほど作ったヘッダーを配置 -->
+    <vue-header></vue-header>
+    <time-line></time-line>
+  </div>
+</template>
+
+<script>
+import VueHeader from './components/Header'
+import TimeLine from './components/TimeLine'
+
+export default {
+  components: {
+    VueHeader,
+    TimeLine
+  }
+}
+</script>
